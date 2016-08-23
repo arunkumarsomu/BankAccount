80ea59cd8caacf2aedec87ab4d55deb76d4c1041
Arun <kumar_sundaram82@yahoo.com>
2016-08-23 16:48:57 -0400

    Fourth

--- BankAccount/src/ssa/Account.java
+++ BankAccount/src/ssa/Account.java
@@ -5,7 +5,7 @@ import java.text.NumberFormat;
 
 public class Account {
 
-	private int accountId ;
+	private int Id ;
 	private static int trackId = 0;
 	private String description;
 	private String type;
@@ -18,7 +18,7 @@ public class Account {
 	
 	public Account(){
 		
-		this.accountId = trackId + 100;
+		this.Id = trackId + 100;
 		this.balance = 0.0;
 		this.transactId = 0;
 		trackId += 100;
@@ -26,7 +26,7 @@ public class Account {
 	}
 		
 	public Account(int actid, String desc){
-		this.accountId = actid;
+		this.Id = actid;
 		this.balance = 0.0;
 		this.transactId = 0;
 		this.setDescription(desc);
@@ -36,7 +36,7 @@ public class Account {
 	public Account(String desc){
 		
 		this.balance = 0.0;
-		this.accountId = trackId + 100;
+		this.Id = trackId + 100;
 		this.transactId = 0;
 		this.setDescription(desc);
 		trackId += 100;
@@ -48,12 +48,12 @@ public class Account {
 		
 	}
 	
-//	private void setAccountId(int acct){
-//		this.accountId = acct;
+//	private void setId(int acct){
+//		this.Id = acct;
 //	}
 	
 	private void setAccountDetails(int actId, String type1, double bal, String firstNam, String lastNam ){
-//		this.accountId = actId;
+//		this.Id = actId;
 		this.type = type1;
 		this.balance = bal;
 		this.firstName = firstNam;
@@ -67,8 +67,8 @@ public class Account {
 		
 	}
 	
-	public int getAccountId(){
-		return this.accountId;
+	public int getId(){
+		return this.Id;
 	}
 	public String getType(){
 		return this.type;
@@ -87,13 +87,13 @@ public class Account {
 	
 	public boolean withdraw(double amt){
 		this.transactId++;
-		String val = "Account :" + this.accountId + " Transaction :"+this.transactId;
+		String val = "Account :" + this.Id + " Transaction :"+this.transactId;
 		
-		System.out.println(" Transaction Id for "+ this.type+" account "+ this.accountId+" is : "+this.transactId);
+	//	System.out.println(" Transaction Id for "+ this.type+" account "+ this.Id+" is : "+this.transactId);
 		
 		if (balance >= amt){
 			balance -= amt;
-			System.out.println("withdraw successfull for amount : "+fmt.format(amt));
+		//	System.out.println("withdraw successfull for amount : "+fmt.format(amt));
 		    val +=  " withdraw successfull for amount : "+fmt.format(amt);
 		    TransLog.put(this.transactId, val);
 			return true;
@@ -110,15 +110,15 @@ public class Account {
 	
 	public void deposit(double amt){
 		this.transactId++;
-		String val = "Account :" + this.accountId + " Transaction :"+this.transactId;
+		String val = "Account :" + this.Id + " Transaction :"+this.transactId;
 		val +=  " Deposit successfull for amount : "+fmt.format(amt);
 		TransLog.put(this.transactId, val);
 		
-		System.out.println(" Transaction Id for "+ this.type+" account "+ this.accountId+" is : "+this.transactId);
-		System.out.println(" Balance before deposit is : "+fmt.format(this.balance));
-		System.out.println(" Deposit amount is : "+fmt.format(amt));
+	//	System.out.println(" Transaction Id for "+ this.type+" account "+ this.Id+" is : "+this.transactId);
+	//	System.out.println(" Balance before deposit is : "+fmt.format(this.balance));
+	//	System.out.println(" Deposit amount is : "+fmt.format(amt));
 		this.balance += amt;
-		System.out.println(" Balance after deposit is : "+fmt.format(this.balance));
+	//	System.out.println(" Balance after deposit is : "+fmt.format(this.balance));
 		
 			
 	}
@@ -139,7 +139,7 @@ public class Account {
 	}
 	
 	public String print(){
-		return ("Account "+this.accountId + " balance is " +fmt.format(this.balance) );
+		return ("Account "+this.Id + " balance is " +fmt.format(this.balance) );
 	}
 	public void displayAccount(){
 		System.out.println(" *************************************************************");
@@ -148,7 +148,7 @@ public class Account {
 		System.out.printf(" \t Acct Type    ");
 		System.out.printf(" \t Account Balance is   ");
 		System.out.println(" ");
-		System.out.printf("    "+this.accountId+"                 "+this.firstName+" "+this.lastName);
+		System.out.printf("    "+this.Id+"                 "+this.firstName+" "+this.lastName);
 		System.out.printf("\t "+ this.type);
 		System.out.printf("\t\t"+ fmt.format(this.balance));
 		System.out.println(" ");
